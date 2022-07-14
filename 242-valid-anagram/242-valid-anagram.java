a2fa6f74f9158f374782a79c601f7f9f673a52af
Varandeep Bhalla <67020950+varan5@users.noreply.github.com>
2022-07-14 05:59:07 +0530

    Time: 3 ms (95.30%), Space: 43.5 MB (70.07%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int sLength = s.length();
+        int tLength = t.length();
+        if (sLength != tLength)
+            return false; 
+        
+        int[] found = new int[26]; // only 26 lowercase english letters
+        for (int i = 0; i < sLength; i++) {
+            char ch = s.charAt(i);
+            found[ch - 'a'] ++; // increament by 1
+        }
+        
+        for (int i = 0; i < tLength; i++) {
+            char ch = t.charAt(i);
+            found[ch - 'a'] --; // decreament by 1
+        }
+        
+        for (int element: found) {
+            if (element != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
