c17efc453822178dd92558449289cbe7629b6a3e
Varandeep Bhalla <67020950+varan5@users.noreply.github.com>
2022-08-05 02:31:13 +0530

    Time: 99 ms (90.55%), Space: 51.3 MB (57.46%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.ts
@@ -0,0 +1,13 @@
+function containsDuplicate(nums: number[]): boolean {
+    const set: Set<number> = new Set();
+    
+    for (let num of nums) {
+        if (set.has(num)) {
+            return true;
+        } else {
+            set.add(num);
+        }
+    }
+    
+    return false;
+};
