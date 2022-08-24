7a5b608f1c45941efbd51dbdd3b19ce5c4fa2084
Varandeep Bhalla <67020950+varan5@users.noreply.github.com>
2022-08-24 17:48:51 +0530

    Time: 1 ms (98.04%), Space: 42.2 MB (63.71%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.java
+++ 771-jewels-and-stones/771-jewels-and-stones.java
@@ -1,11 +1,11 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        int jewelInStoneCount = 0;
+        int count = 0;
         for (char stone: stones.toCharArray()) {
             if (jewels.indexOf(stone) != -1) {
-                jewelInStoneCount ++;
+                count ++;
             }
         }
-        return jewelInStoneCount;
+        return count;
     }
 }
