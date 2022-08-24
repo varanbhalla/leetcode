6400158382bad410a1bad86afdd0b021fdca67bd
Varandeep Bhalla <67020950+varan5@users.noreply.github.com>
2022-08-24 17:35:27 +0530

    Time: 3 ms (96.11%), Space: 41.8 MB (92.48%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
@@ -1,10 +1,9 @@
 class Solution {
     public int mostWordsFound(String[] sentences) {
-        int maxSentenceCount = 0;
-        int currentSentenceCount = 0;
+        int maxSentenceCount = 0, currentSentenceCount = 0;
         for (String sentence: sentences) {
             currentSentenceCount = sentence.split(" ").length;
-            maxSentenceCount = Math.max(maxSentenceCount, currentSentenceCount);
+            maxSentenceCount = Math.max(currentSentenceCount, maxSentenceCount);
         }
         return maxSentenceCount;
     }
