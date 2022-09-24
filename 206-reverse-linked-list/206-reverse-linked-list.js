9144e8e551acf1f63a177e227b9821832c35a39e
Varandeep Bhalla <67020950+varan5@users.noreply.github.com>
2022-09-24 23:15:56 +0530

    Time: 125 ms (9.87%), Space: 44.4 MB (32.91%) - LeetHub

---
+++ 206-reverse-linked-list/206-reverse-linked-list.js
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+var reverseList = function(head) {
+    let first = null;
+    let second = head;
+    let backup = null;
+    
+    while (second !== null) {
+        backup = second.next;
+        second.next = first;
+        first = second;
+        second = backup;
+    }
+    return first;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
